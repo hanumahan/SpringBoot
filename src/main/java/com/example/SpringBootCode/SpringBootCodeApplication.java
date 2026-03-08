c7fe4630aeb49cbe8c59306271fc08cdc1a0a322
hanuma <hanumathkkr2315@gmail.com>
2026-03-08 23:21:42 +0530

    new file

---
+++ src/main/java/com/example/SpringBootCode/SpringBootCodeApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringBootCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCodeApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
