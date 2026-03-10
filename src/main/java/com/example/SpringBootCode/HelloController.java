2e3f419ecc0fdbff2c86d7ef470a2765758d2f2f
hanuma <hanumathkkr2315@gmail.com>
2026-03-10 16:30:15 +0530

    modified version

--- src/main/java/com/example/SpringBootCode/HelloController.java
+++ src/main/java/com/example/SpringBootCode/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.SpringBootCode;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello world to user";
+    @GetMapping("/check")
+    public String check(@RequestParam("val") int val){
+        return val%2==0?"Even":"odd";
     }
 }
